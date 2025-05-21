df781435667d3a037f465756a5ae3829e649b298
Ankit <ankittejyadav@gmail.com>
2025-05-21 10:08:13 -0400

    Fix baseHref and deploy

--- install-github-deps.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing required packages for GitHub XML Manager...
-
-npm install @angular/material
-npm install @angular/cdk
-npm install fast-xml-parser
-
-echo Package installation complete!
-pause
